0004fef46fefe9897a63b6e386bcec71e0b8563d
AngomRanjan <angomranjan@gmail.com>
2024-01-14 03:58:35 +0530

    Render Instruction

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,6 @@
 set -o errexit
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
+./bin/rails assets:precompile
+./bin/rails assets:clean
+./bin/rails db:migrate
